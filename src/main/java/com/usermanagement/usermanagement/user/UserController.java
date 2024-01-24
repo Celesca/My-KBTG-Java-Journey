dff6d05deddd0c240c5851120d1a55ced6c5e921
Sawit Koseeyaumporn | Folk <121187906+Celesca@users.noreply.github.com>
2024-01-24 23:24:58 +0700

    ADD: User management CRUD with Spring

---
+++ src/main/java/com/usermanagement/usermanagement/user/UserController.java
@@ -0,0 +1,76 @@
+package com.usermanagement.usermanagement.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController() {
+        this.userService = new UserService();
+    }
+    @GetMapping("api/users")
+    public List<User> getUsers(@RequestParam("active") Optional<Boolean> active) {
+        return userService.getUsers(active);
+    }
+
+    @PostMapping("api/users")
+    public User createUser(@RequestBody UserRequest request ) {
+        return userService.createUser(request);
+
+    }
+
+    @PutMapping("api/users/{id}")
+    public void editUser(@PathVariable("id") int id, @RequestBody UserRequest request) {
+        userService.editUser(id, request);
+
+    }
+
+    @DeleteMapping("api/users/{id}")
+    public void deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+    }
+
+    // Contact ที่เรา Defined ไว้เลย
+    record UserRequest(String name, int age) {}
+
+}
+
+class User {
+    private int id;
+    private String name;
+    private int age;
+    private Boolean active;
+
+    // Constructor
+    public User(int id, String name, int age, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/usermanagement/usermanagement/user/UserService.java
@@ -0,0 +1,61 @@
+package com.usermanagement.usermanagement.user;
+
+import com.usermanagement.usermanagement.mail.GoogleMailService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+public class UserService {
+
+
+
+
+    List<User> users = new ArrayList<>(
+            List.of(
+                    new User(1, "John", 22, false),
+                    new User(2, "Jane", 21, true),
+                    new User(3, "Mary", 25, false)
+            )
+    );
+
+    private GoogleMailService googleMailService;
+
+    public UserService() {
+        this.googleMailService = new GoogleMailService();
+        this.googleMailService.setUrl("mail.google.com");
+        this.googleMailService.setPort("42");
+    }
+
+    public List<User> getUsers(Optional<Boolean> active) {
+        if (active.isPresent()) {
+            return users.stream().filter(u -> u.getActive() == active.get()).toList();
+        }
+        return users;
+    }
+
+    public User createUser(UserController.UserRequest request) {
+        Optional<Integer> maxId = users.stream().map(User::getId).max(Integer::compareTo);
+        int nextId = maxId.orElse(0) + 1;
+        User user = new User(nextId, request.name() , request.age() , true);
+        users.add(user);
+
+        // TODO: send the mail
+        googleMailService.sentEmail("dev@gmail.com" , "New user created");
+        return user;
+    }
+
+    public void editUser(int id, UserController.UserRequest request)  {
+        Optional<User> user = users.stream().filter(u -> u.getId() == id).findFirst();
+        if (user.isPresent()){
+            User u = user.get();
+            u.setName(request.name());
+        }
+    }
+
+    public void deleteUser(int id) {
+        users.removeIf(u -> u.getId() == id);
+    }
+
+}
