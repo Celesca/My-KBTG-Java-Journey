2f42c7e7edf342cc4dc3d2fc5f4e4bbe8a6a2070
Sawit Koseeyaumporn | Folk <folk.sawit@gmail.com>
2024-02-01 13:45:22 +0700

    FEAT: exception-handling

--- src/main/java/com/usermanagement/usermanagement/exception/NotFoundException.java
+++ src/main/java/com/usermanagement/usermanagement/exception/NotFoundException.java
@@ -3,7 +3,7 @@ package com.usermanagement.usermanagement.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+//@ResponseStatus(value = HttpStatus.NOT_FOUND) - ไม่ต้องเขียนก็ได้
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
